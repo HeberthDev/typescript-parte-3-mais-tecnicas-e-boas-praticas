f164660c96d3e75fc19110e355bbbdca325a4554
HeberthDev <heberthdeveloper@gmail.com>
2024-10-21 08:08:43 -0300

    Escapando texto através de Decorator

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,7 +1,10 @@
+import { escapar } from "../decorators/escapar.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
+
+    @escapar
     // Está como protected também, igual a classe "mãe" View, porquê a classe filha se não colocar nada, de forma implícita é public. Então a classe filha está mudando um método protected pra public. Por isso que temos que colocar "protected" nas classes filhas também para não ser visto por outro lugares que não poderia ver sem necessidade.
     protected template( model: Negociacoes ): string {
         return `
