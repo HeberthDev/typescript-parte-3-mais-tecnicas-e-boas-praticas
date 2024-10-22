068c694970e0e9833a9f88151bf2b10c98741c1b
HeberthDev <heberthdeveloper@gmail.com>
2024-10-22 07:42:33 -0300

    Utilizando Object.defineProperty

--- app/decorators/dom-injector.ts
+++ app/decorators/dom-injector.ts
@@ -2,7 +2,14 @@ export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
         const getter = function() {
             const elemento = document.querySelector(seletor);
+            console.log(`Buscando elemento do dom com o seletor ${seletor} para injetar em ${propertyKey}`);
             return elemento;
         }
+
+        Object.defineProperty(
+            target,
+            propertyKey,
+            {get: getter}
+        );
     }
 }
--- dist/js/decorators/dom-injector.js
+++ dist/js/decorators/dom-injector.js
@@ -2,7 +2,9 @@ export function domInjector(seletor) {
     return function (target, propertyKey) {
         const getter = function () {
             const elemento = document.querySelector(seletor);
+            console.log(`Buscando elemento do dom com o seletor ${seletor} para injetar em ${propertyKey}`);
             return elemento;
         };
+        Object.defineProperty(target, propertyKey, { get: getter });
     };
 }
