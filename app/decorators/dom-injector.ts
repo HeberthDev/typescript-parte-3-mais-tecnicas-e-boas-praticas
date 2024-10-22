845604ff50618c10bf3b5d3e936699f999cb627f
HeberthDev <heberthdeveloper@gmail.com>
2024-10-22 07:55:48 -0300

    Aplicando cache decorator copy

--- app/decorators/dom-injector.ts
+++ app/decorators/dom-injector.ts
@@ -1,8 +1,13 @@
 export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
+        let elemento: HTMLElement;
+
         const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`Buscando elemento do dom com o seletor ${seletor} para injetar em ${propertyKey}`);
+            if( !elemento ) {
+                elemento = <HTMLElement>document.querySelector( seletor );
+                console.log(`Buscando elemento do dom com o seletor ${seletor} para injetar em ${propertyKey}`);
+            }
+
             return elemento;
         }
 
--- dist/js/decorators/dom-injector.js
+++ dist/js/decorators/dom-injector.js
@@ -1,8 +1,11 @@
 export function domInjector(seletor) {
     return function (target, propertyKey) {
+        let elemento;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
-            console.log(`Buscando elemento do dom com o seletor ${seletor} para injetar em ${propertyKey}`);
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`Buscando elemento do dom com o seletor ${seletor} para injetar em ${propertyKey}`);
+            }
             return elemento;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
