0395eca11383ecad2eed881e4d482b8d195a3ac5
HeberthDev <heberthdeveloper@gmail.com>
2024-10-24 07:49:07 -0300

    Evitando importar negociações duplicadas

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -42,4 +42,10 @@ export class Negociacao implements Implimivel {
             Valor: ${this.valor}
         `;
     }
+
+    public ehIgual( negociacao: Negociacao ): boolean {
+        return this.data.getDate() === negociacao.data.getDate()
+            && this.data.getMonth() === negociacao.data.getMonth()
+            && this.data.getFullYear() === negociacao.data.getFullYear();
+    }
 }
